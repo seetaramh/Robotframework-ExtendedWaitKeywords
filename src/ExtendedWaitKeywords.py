419d745bd3e67aa9021ec0c191a118a2ae5bd91a
s.hegde <s.hegde@devon.nl>
2018-05-22 16:15:49 +0530

    Added comments for each keyword

--- src/ExtendedWaitKeywords.py
+++ src/ExtendedWaitKeywords.py
@@ -1,9 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#    Extended Wait Keywords - extended keywords for Robot framework to handle AngularJS support.
+#    Copyright (c) 2018 Seetaram Hegde <seetaramh@users.noreply.github.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from robot.libraries.BuiltIn import BuiltIn
 from selenium.webdriver.support.ui import WebDriverWait
 from SeleniumLibrary.base import LibraryComponent
 from time import sleep
 from timeit import itertools
-
 
 
 class ExtendedWaitKeywords(LibraryComponent):
@@ -12,6 +31,10 @@ class ExtendedWaitKeywords(LibraryComponent):
 
     def __init__(self):
         pass
+
+    # Waits until AngularJS screen is loaded. 
+    # This keyword is useful when the page is built with angularJS
+    # Example: Wait Until Angular Ready 60s
 
     def wait_until_angular_ready(self, timeout=None):
         timeout = int(self.get_timeout(timeout))
@@ -25,12 +48,21 @@ class ExtendedWaitKeywords(LibraryComponent):
         self._wait_until_jquery_ready(timeout)
         
 
-     
+    # Waits until the JScript is ready
+    # Example: Wait Until Jscript Ready 10s
+      
     def wait_until_jscript_ready(self,timeout=None):
         self._wait_until_jscript_ready(timeout)
+
+    # Waits until the Jquery is ready
+    # Example: Wait Until Jquery Ready 10s
         
     def wait_until_jquery_ready(self,timeout=None):
         self._wait_until_jquery_ready(timeout)
+
+    # Waits until new window is opened. This keyword comes handy when the second tab of the browser takes
+    # time to open.
+    # Example: Wait For New Window 10s
 
     def wait_for_new_window(self, handles_before, timeout=None):
         timeout = int(self.get_timeout(timeout))
